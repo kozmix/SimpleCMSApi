7de0203173c94b0d4eb9023892ad15eede81cf4b
Lorant Kozma <lorant.kozma@gmail.com>
2018-05-01 14:23:27 +0300

    Updates with Models and Database Context

---
+++ SimpleCMSApi/Data/SimpleCmsApiContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SimpleCMSApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleCMSApi.Data
+{
+    public class SimpleCmsApiContext : DbContext
+    {
+        public SimpleCmsApiContext(DbContextOptions<SimpleCmsApiContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Page> Pages { get; set; }
+        public DbSet<Sidebar> Sidebar { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ SimpleCMSApi/Models/Sidebar.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleCMSApi.Models
+{
+    public class Sidebar
+    {
+        public int ID { get; set; }
+        public string Content { get; set; }
+    }
+}
